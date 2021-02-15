5cd1800b3727f4d658c705e97521b0b4b66eeabc
Ali Germiyanoglu <aligermiyanoglu@gmail.com>
2021-02-15 20:07:13 +0000

    Initial Commit

---
+++ Logins/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Logins
+//
+//  Created by CTW00708-Admin on 15/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
