cf341308c71f3a550cf5630db6e576e57f50c0f7
Ali Germiyanoglu <aligermiyanoglu@gmail.com>
2021-02-15 23:13:14 +0000

    User model introduced

---
+++ Logins/Data/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Logins
+//
+//  Created by CTW00708-Admin on 15/02/2021.
+//
+
+import Foundation
+
+struct User: Decodable {
+    let id: String
+    let name: String
+    let username: String
+    let email: String
+}
